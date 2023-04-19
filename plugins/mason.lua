651c9a6247f177d8f631bff933ef80208aa28431
Averanthia <averanthia@proton.me>
2023-04-19 02:53:01 +0200

    Remove cmake for now

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "lua_ls", "omnisharp", "rust_analyzer", "clangd", "cmake" },
+      ensure_installed = { "lua_ls", "omnisharp", "rust_analyzer", "clangd" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
